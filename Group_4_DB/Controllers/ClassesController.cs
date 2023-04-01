cc9a43d3cd427aa84b8eada31a4a8c55c361367a
Mjpilgrim1994 <mattpilgrim1994@gmail.com>
2023-03-31 21:20:43 -0500

    Working

---
+++ Group_4_DB/Controllers/ClassesController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Group_4_DB.Data;
+using Group_4_DB.Models;
+
+namespace Group_4_DB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClassesController : ControllerBase
+    {
+        private readonly academic_settingsContext _context;
+
+        public ClassesController(academic_settingsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Classes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Classes>>> GetClasses()
+        {
+          if (_context.Classes == null)
+          {
+              return NotFound();
+          }
+            return await _context.Classes.ToListAsync();
+        }
+
+        // GET: api/Classes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Classes>> GetClasses(string id)
+        {
+          if (_context.Classes == null)
+          {
+              return NotFound();
+          }
+            var classes = await _context.Classes.FindAsync(id);
+
+            if (classes == null)
+            {
+                return NotFound();
+            }
+
+            return classes;
+        }
+
+        // PUT: api/Classes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutClasses(string id, Classes classes)
+        {
+            if (id != classes.ClassId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(classes).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClassesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Classes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Classes>> PostClasses(Classes classes)
+        {
+          if (_context.Classes == null)
+          {
+              return Problem("Entity set 'academic_settingsContext.Classes'  is null.");
+          }
+            _context.Classes.Add(classes);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ClassesExists(classes.ClassId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetClasses", new { id = classes.ClassId }, classes);
+        }
+
+        // DELETE: api/Classes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClasses(string id)
+        {
+            if (_context.Classes == null)
+            {
+                return NotFound();
+            }
+            var classes = await _context.Classes.FindAsync(id);
+            if (classes == null)
+            {
+                return NotFound();
+            }
+
+            _context.Classes.Remove(classes);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ClassesExists(string id)
+        {
+            return (_context.Classes?.Any(e => e.ClassId == id)).GetValueOrDefault();
+        }
+    }
+}
--- Group_4_DB/Program.cs
+++ Group_4_DB/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<Group_4_DB.Data.academic_settingsContext>(
+    options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("academic_settings"));
+    });
+
+builder.Services.AddMvc(option => option.EnableEndpointRouting = false)
+    .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
 
 var app = builder.Build();
 
